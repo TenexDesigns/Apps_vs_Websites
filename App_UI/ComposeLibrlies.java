79348f4f8f47f49d40a119f002e0b4e306ee731e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-16 09:54:47 +0300

    Create ComposeLibrlies.java

---
+++ App_UI/ComposeLibrlies.java
@@ -0,0 +1,47 @@
+
+
+
+Compose Dialogs: This library provides a number of components for creating dialogs in Jetpack Compose apps.  ---> https://github.com/maxkeppeler/sheets-compose-dialogs
+material 3 componennt - https://m3.material.io/develop/android/mdc-android
+Icons -Yes, there are a number of icon libraries that you can use in Jetpack Compose. Here are a few examples:
+
+Material Icons: This is the default icon library for Jetpack Compose. It provides a wide range of icons that are designed to match the Material Design aesthetic.
+Material Icons Extended: This library provides a set of extended Material Design icons that are not included in the default Material Icons library.
+
+
+
+  You can also use any custom icon that you create yourself. To do this, you can convert your icon to a vector image and then add it to your project's resources directory.
+
+To use an icon in Jetpack Compose, you can use the Icon composable. This composable takes the name of the icon as a parameter, and it will display the icon on the screen.
+
+For example, to display the Material Icons Menu icon, you would use the following code:
+
+import androidx.compose.material.Icon
+
+@Composable
+fun MyComposable() {
+    Icon(Icons.Filled.Menu)
+}
+You can also use the Icon composable to display custom icons. To do this, you need to pass the painter parameter to the Icon composable. The painter parameter is a drawable that represents the icon.
+
+For example, to display a custom icon that is stored in the resources directory, you would use the following code:
+
+import androidx.compose.ui.res.painterResource
+
+@Composable
+fun MyComposable() {
+    Icon(painterResource(R.drawable.my_icon))
+}
+I hope this helps!
+
+
+
+
+
+
+
+
+  
+
+
+  
